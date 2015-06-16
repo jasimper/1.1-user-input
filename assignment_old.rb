2fe2ec15c6f6c05ae362247408cbf47cb74271fd
Jamie Simper <fromraintohope@gmail.com>
2015-06-15 20:46:02 -0400

    Reworked whole assignment

--- assignment_old.rb
+++ assignment_old.rb
@@ -1,10 +1,6 @@
-name = ""
-	until name.length >= 1
-		puts "Please enter your name."
+puts "What's your name?"
 	name = gets.chomp
-end
-
-	puts %{What is your ideal legal status?
+puts %{What is your ideal legal status?
 Please answer by entering the cooresponding number.
 1. I am law abiding!
 2. I make sure others obey the law.
@@ -12,14 +8,14 @@ Please answer by entering the cooresponding number.
 4. Questionable.
 }
 
-legal = 0
-	until [1, 2, 3, 4].include? legal do
+# Not working as expected
+	legal = gets.chomp.to_i
+	if legal.include?(1, 2, 3, 4)
 		puts "Please enter a number between 1 and 4."
-		legal = gets.chomp.to_i
 	end
 
-	if
-	  legal == 1
+if
+legal == 1
 	puts "The Empire thanks you for your aquiescence, citizen #{name}."
 elsif legal == 2
 	puts "#{name}, aren't you a little short for a Storm Trooper?"
@@ -27,4 +23,6 @@ elsif legal == 3
 	puts "Hey, #{name}! Who's scruffy-lookin'?"
 elsif legal == 4
 	puts "#{name}, I loathe bounty hunters. We don't need their scum..."
+else
+	puts "#{name}, meesa Jar Jar Binks."
 end
